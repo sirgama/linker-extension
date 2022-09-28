05192bb62c196b46426ca500ab377bfe500e054e
sirgama <sirengogamaliel@gmail.com>
2022-09-28 23:27:54 +0300

    add emojis to make the homepage more interactive

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,8 +4,8 @@ let getStartedBtn = document.getElementById("start")
 // Function to change content of the button when the mouse hovers over it
 
 function hoverEffect(){
-    getStartedBtn.textContent = "Let's do this!"
+    getStartedBtn.textContent = "Let's do this!🦁"
 }
 function outEffect(){
-    getStartedBtn.textContent = "Get started"
+    getStartedBtn.textContent = "Get started 🐥"
 }
