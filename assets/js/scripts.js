8ecc3f249f0291df60bb5c58026980b4faecb16e
sirgama <sirengogamaliel@gmail.com>
2022-09-28 23:17:49 +0300

    add funtin to change get started text on hover

---
+++ assets/js/scripts.js
@@ -0,0 +1,11 @@
+
+let getStartedBtn = document.getElementById("start")
+
+// Function to change content of the button when the mouse hovers over it
+
+function hoverEffect(){
+    getStartedBtn.textContent = "Let's do this!"
+}
+function outEffect(){
+    getStartedBtn.textContent = "Get started"
+}
